93f066774dfb858ed1629044df7ae65810b4b071
Elias Nau <contact@eliasnau.dev>
2026-02-24 18:10:36 +0100

    feat: add /docs redirect

--- apps/docs/src/app/(home)/page.tsx
+++ apps/docs/src/app/(home)/page.tsx
@@ -1,14 +1,16 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
+  return redirect("/docs");
   return (
     <div className="flex flex-col justify-center text-center flex-1">
       <h1 className="text-2xl font-bold mb-4">Hello World</h1>
       <p>
-        You can open{' '}
+        You can open{" "}
         <Link href="/docs" className="font-medium underline">
           /docs
-        </Link>{' '}
+        </Link>{" "}
         and see the documentation.
       </p>
     </div>
